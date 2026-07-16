d1b5a4848d7d7a383aa7bc77ae1a20b065362d0c
Zendi Iman <zendiiman6@gmail.com>
2026-07-16 15:49:17 +0700

    Ready to Deploy

--- new_luwes/lang/id/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'failed'   => 'Email/username atau password salah.',
-    'password' => 'Password yang dimasukkan tidak benar.',
-    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
-
-];
